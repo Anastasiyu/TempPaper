4ad96ab39a906cb36bc673d1d4dee7f63b362a46
Anastasiyu <asy.safiulina@mail.ru>
2022-09-10 16:57:54 +0300

    Книга учета данные

--- src/Empoyee.java
+++ src/Empoyee.java
@@ -1,2 +1,68 @@
 public class Empoyee {
+
+    private int id;
+    private String name;
+    private String midleName;
+    private String surName;
+    private int department;
+    private double salary;
+    private static int idCounter = 0;
+
+    public Empoyee() {
+        this.id = ++idCounter;
+    }
+
+    public Empoyee(String surName, String name, String midleName) {
+        this.id = ++idCounter;
+        this.name = name;
+        this.midleName = midleName;
+        this.surName = surName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMidleName() {
+        return midleName;
+    }
+
+    public void setMidleName(String midleName) {
+        this.midleName = midleName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    @Override
+    public String toString() {
+        return  " " + idCounter + "/ Ф.И.О.: " + surName + " " + name + " " + surName + ", номер отдела- " + department + ". Сумма зарплаты: " + salary +"рублей.";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет!");
+        Empoyee empoyee = new Empoyee("Иванов", "Иван", "Иванович");
+        empoyee.setDepartment (1);
+        empoyee.setSalary (44500.00);
+        System.out.println(empoyee);
+        Empoyee empoyee2 = new Empoyee("Сидорова", "Ольга", "Олеговна");
+        empoyee2.setDepartment (3);
+        empoyee2.setSalary (32400.30);
+        System.out.println(empoyee2);
     }
 }
